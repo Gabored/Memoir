f20ba3fd9a26169f447cf825607fd5cf3545e235
Gabriel Olvera <gavoxolvera@gmail.com>
2023-10-29 15:35:14 -0600

    first index.js , project starts

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Gabriel Olvera, Marian Sedano
+
+// Memoir Project 
+
+const express = require ('express');
+
+ // const routes = require('./routes');
+
+const app = express();
+
+
+// const mongoose = require('mongoose');
+
+const mongourl = "mongo url"
+
+// app.use('',routes)
+
+app.get('', (req, res) => {
+    res.send('api works!');
+})
+
+app.listen (3000, ()=>{ // Set Up server at LocalHost 3000
+    console.log('app is running');
+
+})
+
